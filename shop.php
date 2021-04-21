ac0a5380df802b2450ba2812824c1c7f40e452b6
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-04-20 22:32:21 -0400

    initial changes to shop.php

--- shop.php
+++ shop.php
@@ -1,1 +1,38 @@
-epic shop
+<?php
+
+	//opens mysqli connection
+	$mysqli = new mysqli('localhost', 'root', '', 'FantasyShop');
+	//error handler
+	if ($mysqli->connect_error) {
+		die("Connection failed: " . $mysqli->connect_error);
+	}
+
+	//sql query
+ $sql = 'SELECT * FROM product';
+	$stmt = $mysqli->prepare($sql);
+	$stmt->execute();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+ <?php
+		//lloping through results 	
+		while($stmt->fetch()) {
+			echo $db_product . ' ';
+	}
+	//closing connections
+		$stmt->close();
+		$mysqli->close(); 
+	?>
+
+</body>
+</html>
