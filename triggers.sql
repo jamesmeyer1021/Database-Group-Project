13bef424d221887b03242e7b13b8b99276a3e5db
jshea4 <77816466+jshea4@users.noreply.github.com>
2021-04-25 18:21:40 -0400

    fixed indentation

--- triggers.sql
+++ triggers.sql
@@ -4,7 +4,7 @@ FOR EACH ROW
 BEGIN
     UPDATE product 
     SET product.product_quantity = product.product_quantity - 1 
-	  WHERE product.product_id = NEW.product_id;
+    WHERE product.product_id = NEW.product_id;
 END //
 DELIMITER ;
 
@@ -12,9 +12,9 @@ DELIMITER //
 CREATE TRIGGER on_deletion AFTER DELETE ON cart
 FOR EACH ROW
 BEGIN
-	  UPDATE product 
-  	SET product.product_quantity = product.product_quantity + (OLD.product_quantity) 
-	  WHERE product.product_id = OLD.product_id;
+    UPDATE product 
+    SET product.product_quantity = product.product_quantity + (OLD.product_quantity) 
+    WHERE product.product_id = OLD.product_id;
 END //
 DELIMITER ;
 
@@ -22,10 +22,10 @@ DELIMITER //
 CREATE TRIGGER on_update AFTER UPDATE ON cart 
 FOR EACH ROW
 BEGIN
-  	DECLARE update_dif integer;
-  	SET update_dif = NEW.product_quantity - OLD.product_quantity;
+    DECLARE update_dif integer;
+    SET update_dif = NEW.product_quantity - OLD.product_quantity;
     UPDATE product
-  	SET product.product_quantity = product.product_quantity - update_dif
+    SET product.product_quantity = product.product_quantity - update_dif
     WHERE product.product_id = NEW.product_id;
 END //
 DELIMITER ;
