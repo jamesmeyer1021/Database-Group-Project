102cb3aea46a46008c6d60eff400899c208f86d6
ashelto6 <60279886+ashelto6@users.noreply.github.com>
2021-04-25 23:04:05 -0400

    filter feature implemented

--- shop.php
+++ shop.php
@@ -1,29 +1,67 @@
 <?php
-	session_start();
-	//If user isn't logged in, send them back to the login page
-	if (empty($_SESSION['uid'])) {
-		header('Location:index.php');
-	}
+session_start();
+//If user isn't logged in, send them back to the login page
+if (empty($_SESSION['uid'])) {
+	header('Location:index.php');
+}
 
+//opens mysqli connection
+$mysqli = new mysqli('localhost', 'root', '', 'FantasyShop');
 
-	//opens mysqli connection
-	$mysqli = new mysqli('localhost', 'root', '', 'FantasyShop');
+//error handler
+if ($mysqli->connect_error) {
+	die("Connection failed: " . $mysqli->connect_error);
+}
 
-	//error handler
-	if ($mysqli->connect_error) {
-		die("Connection failed: " . $mysqli->connect_error);
-	}
+if(isset($_POST['price'])) {	if($_POST['price']=='range1'){
+	$low = 200; $high = 300;
+}
+if($_POST['price']=='range2'){
+	$low = 300; $high = 400;
+}
+if($_POST['price']=='range3'){
+	$low = 400; $high = 500;
+} }
 
-	//sql query
- 	$sql = 'SELECT * FROM product';
+//sql query
+if(isset($_POST['type'])) { 
+	$type = $_POST['type'];
+	$sql = "SELECT * FROM product WHERE product_type =?";
 	$stmt = $mysqli->prepare($sql);
-	$stmt->execute();
-	$stmt->bind_result($db_product_id, $db_product_name, $db_product_type, $db_product_brand, $db_product_quantity, $db_product_price);
+	$stmt->bind_param('s', $type);
+} 
+elseif (isset($_POST['brand'])) {
+	$brand= $_POST['brand'];
+	$sql = "SELECT * FROM product WHERE product_brand =?";
+	$stmt = $mysqli->prepare($sql);
+	$stmt->bind_param('s', $brand);
+} 
+elseif  (isset($_POST['price'])) {
+	if($_POST['price']=='range1'){
+		$low = 200; $high = 300;
+	}
+	if($_POST['price']=='range2'){
+		$low = 300; $high = 400;
+	}
+	if($_POST['price']=='range3'){
+		$low = 400; $high = 500;
+	} 
+	$sql= "SELECT * FROM product WHERE product_price BETWEEN ? AND ?";
+	$stmt = $mysqli->prepare($sql);
+	$stmt->bind_param('ii', $low, $high);
+}
+else{
+	$sql = 'SELECT * FROM product';
+	$stmt = $mysqli->prepare($sql);
+}
 
-	$output = '';
-	//setting titles
-	$output .= '<table>';
-		$output .= '	<tr> 
+$stmt->execute();
+$stmt->bind_result($db_product_id, $db_product_name, $db_product_type, $db_product_brand, $db_product_quantity, $db_product_price);
+
+$output = '';
+//setting titles
+$output .= '<table>';
+$output .= '	<tr> 
 				<th> Product ID </th>
 				<th> Product Name </th>
 				<th> Product Type </th>
@@ -31,91 +69,100 @@
 				<th> In Stock </th>
 				<th> Price </th>
 				</tr>';
-	//looping through results 	
-	while($stmt->fetch()) {
-		//if product is not out of stock
-		if($db_product_quantity > 0) {
-			$output .= '<tr>';
-			$output .= '<td>' . $db_product_id . '</td><td>' . $db_product_name . '</td><td>' . $db_product_type . '</td><td>' . $db_product_brand . '</td><td>' . $db_product_quantity . '</td><td>$' . $db_product_price .  '</td>';
-			$output .= '<td><button onclick="addToCart('.$db_product_id.')">Add To Cart</button> </td>';
-			$output .= '</tr>';
-		}
-	
+//looping through results 	
+while ($stmt->fetch()) {
+	//if product is not out of stock
+	if ($db_product_quantity > 0) {
+		$output .= '<tr>';
+		$output .= '<td>' . $db_product_id . '</td><td>' . $db_product_name . '</td><td>' . $db_product_type . '</td><td>' . $db_product_brand . '</td><td>' . $db_product_quantity . '</td><td>$' . $db_product_price .  '</td>';
+		$output .= '<td><button onclick="addToCart(' . $db_product_id . ')">Add To Cart</button> </td>';
+		$output .= '</tr>';
 	}
-	$output .= '</table>';
+}
+$output .= '</table>';
 
-	//closing connections
-	$stmt->close();
-	$mysqli->close();
+//closing connections
+$stmt->close();
+$mysqli->close();
 ?>
-
-
-
-
-
 
 <!-- HTML SECTION -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <title>Fantasy Shop</title>
 
-<style>
-table, th, td {
-	border: 1px solid black;
-	padding: 5px;
-}
-</style>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<title>Fantasy Shop</title>
+
+	<style>
+		table,
+		th,
+		td {
+			border: 1px solid black;
+			padding: 5px;
+		}
+	</style>
 </head>
 
 <body>
-	<center><p>From below dropdown menus you can apply filters</p><center>
-  <!--filter forms-->
-  <form action="" method="post">
-            <select name="type">
-                <option value="">Select Type</option>
-                <option value="Weapon">Weapon</option>
-            </select>
-            <select name="brand">
-                <option value ="">Select Brand</option>
-                <option value="Halo">Halo</option>
-                <option value="Kingdom Hearts">Kingdom Hearts</option>
-																<option value="Zelda">Zelda</option>
-																<option value="Mario">Mario</option>
-																<option value="Activision">Activision</option>
-            </select>
-            <select name="price">
-                <option value="">Select Price Range</option>
-                <option value="range1">200 - 300</option>
-                <option value="range2">300 - 400</option>
-																<option value="range3">400 - 500</option>
-            </select>
-												<button type="submit" class="btn btn-primary">Apply</button>
-  </form>
+	<center>
+		<p>From below dropdown menus you can apply filters</p>
+		<center>
+			<!--filter forms-->
+			<form action="" method="post">
+				<select name="type">
+					<option value="">Select Type</option>
+					<option value="Weapon">Weapon</option>
+					<option value="Weapon">Potion</option>
+				</select>
+				<button type="submit">Apply</button>
+			</form>
 
-	<h2>Products</h2>
-	<p ><a href="cart.php">View cart</a></p>
-	<?php echo $output; ?>
+			<form action="" method="post">
+				<select name="brand">
+					<option value="">Select Brand</option>
+					<option value="Halo">Halo</option>
+					<option value="Kingdom Hearts">Kingdom Hearts</option>
+					<option value="Zelda">Zelda</option>
+					<option value="Mario">Mario</option>
+					<option value="Activision">Activision</option>
+				</select>
+				<button type="submit">Apply</button>
+			</form>
 
-	<script>
-		function addToCart(id) {
-			const data = {
-				product_id: id,
-				uid: <?php echo $_SESSION['uid']; ?>
-			};
-			$.ajax({
-				type: "POST",
-				url: "./addToCart.php",
-				data: data,
-				success: function (data, status) {
-					console.log(status);
+			<form action="" method="post">
+				<select name="price">
+					<option value="">Select Price Range</option>
+					<option value="range1">200 - 300</option>
+					<option value="range2">300 - 400</option>
+					<option value="range3">400 - 500</option>
+				</select>
+				<button type="submit">Apply</button>
+			</form>
+
+			<h2>Products</h2>
+			<p><a href="cart.php">View cart</a></p>
+			<?php echo $output; ?>
+
+			<script>
+				function addToCart(id) {
+					const data = {
+						product_id: id,
+						uid: <?php echo $_SESSION['uid']; ?>
+					};
+					$.ajax({
+						type: "POST",
+						url: "./addToCart.php",
+						data: data,
+						success: function(data, status) {
+							console.log(status);
+						}
+					});
 				}
-			});
-		}
-	</script>
+			</script>
 </body>
+
 </html>
