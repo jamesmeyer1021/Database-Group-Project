0f309bad98212b2bd0dd12dbe00d46dccdd38a72
jshea4 <77816466+jshea4@users.noreply.github.com>
2021-04-26 15:58:05 -0400

    Triggers bug fixes

--- triggers.sql
+++ triggers.sql
@@ -1,30 +1,31 @@
-Delimiter//
-create trigger on_insert after insert on cart
-for each row
-begin
-update product 
-set product.product_quantity - 1 
-where cart.product_id = product.product_id;
-end//
-Delimiter;
+DELIMITER //
+CREATE TRIGGER on_insert AFTER INSERT ON cart
+FOR EACH ROW
+BEGIN
+	UPDATE product 
+	SET product.product_quantity = product.product_quantity - 1 
+	WHERE product.product_id = NEW.product_id;
+END //
+DELIMITER ;
 
-Delimiter//
-create trigger on_deletion after delete on cart
-for each row
-begin 
-update product 
-set product.product_quantity+(select product_quantity from cart) 
-where cart.product_id = product.product_id;
-end//
-Delimiter;
+DELIMITER //
+CREATE TRIGGER on_deletion AFTER DELETE ON cart
+FOR EACH ROW
+BEGIN
+	UPDATE product 
+	SET product.product_quantity = product.product_quantity + (OLD.product_quantity) 
+	WHERE product.product_id = OLD.product_id;
+END //
+DELIMITER ;
 
-Delimiter//
-create trigger on_update after update on cart 
-for each row
-begin
-update product
-declare update_dif integer;
-set update_dif= new.product_quantity-old.product_quantity
-set product.product_quantity= product_quantity - update_dif
-end//
-Delimiter;
+DELIMITER //
+CREATE TRIGGER on_update AFTER UPDATE ON cart 
+FOR EACH ROW
+BEGIN
+	DECLARE update_dif integer;
+	SET update_dif = NEW.product_quantity - OLD.product_quantity;
+	UPDATE product
+	SET product.product_quantity = product.product_quantity - update_dif
+	WHERE product.product_id = NEW.product_id;
+END //
+DELIMITER ;
