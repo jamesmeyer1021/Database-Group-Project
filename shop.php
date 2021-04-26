3ba3cde73f85c59e4dd83c08215073adf02b77e8
jshea4 <jasonshea55@gmail.com>
2021-04-26 18:10:04 -0400

    Added location filter

--- shop.php
+++ shop.php
@@ -22,37 +22,36 @@ if(isset($_POST['type'])) {
 	$stmt = $mysqli->prepare($sql);
 	$stmt->bind_param('s', $type);
 } 
-elseif (isset($_POST['brand'])) {
+else if (isset($_POST['brand'])) {
 	$brand= $_POST['brand'];
 	$sql = "SELECT * FROM product WHERE product_brand =?";
 	$stmt = $mysqli->prepare($sql);
 	$stmt->bind_param('s', $brand);
 } 
-elseif  (isset($_POST['price'])) {
-	if($_POST['price']=='range1'){
-		$low = 0; $high = 100;
+else if (isset($_POST['price'])) {
+	$low = -999999;
+	$high = 999999;
+	if(!empty($_POST['low'])){
+		$low = $_POST['low'];
 	}
-
-	if($_POST['price']=='range2'){
-		$low = 100; $high = 200;
+	if (!empty($_POST['high'])) {
+		$high = $_POST['high'];
 	}
-	if($_POST['price']=='range3'){
-		$low = 200; $high = 300;
-	} 
-	if($_POST['price']=='range4'){
-		$low = 300; $high = 400;
-	}
-	if($_POST['price']=='range5'){
-		$low = 400; $high = 500;
-	} 
-	if($_POST['price']=='range6'){
-		$low = 500; $high = 600;
-	} 
+ 
 	$sql= "SELECT * FROM product WHERE product_price BETWEEN ? AND ?";
 	$stmt = $mysqli->prepare($sql);
 	$stmt->bind_param('ii', $low, $high);
 }
-else{
+else if (isset($_POST['location'])) {
+	$location = $_POST['location'];
+	$sql = 'SELECT product_id, product_name, product_type, product_brand, product_quantity, product_price, product.location_id
+			FROM product
+			JOIN location ON product.location_id = location.location_id
+			WHERE location_name = ?';
+	$stmt = $mysqli->prepare($sql);
+	$stmt->bind_param('s', $location);
+}
+else {
 	$sql = 'SELECT * FROM product';
 	$stmt = $mysqli->prepare($sql);
 }
@@ -64,7 +63,6 @@ $output = '';
 //setting titles
 $output .= '<table>';
 $output .= '<tr> 
-		<th> Product ID </th>
 		<th> Product Name </th>
 		<th> Product Type </th>
 		<th> Brand </th>
@@ -76,8 +74,8 @@ while ($stmt->fetch()) {
 	//if product is not out of stock
 	if ($db_product_quantity > 0) {
 		$output .= '<tr>';
-		$output .= '<td>' . $db_product_id . '</td><td>' . $db_product_name . '</td><td>' . $db_product_type . '</td><td>' . $db_product_brand . '</td><td>' . $db_product_quantity . '</td><td>$' . $db_product_price .  '</td>';
-		$output .= '<td><button onclick="addToCart(' . $db_product_id . ')">Add To Cart</button> </td>';
+		$output .= '<td>' . $db_product_name . '</td><td>' . $db_product_type . '</td><td>' . $db_product_brand . '</td><td>' . $db_product_quantity . '</td><td>$' . $db_product_price .  '</td>';
+		$output .= '<td><button id="btn-'.$db_product_id.'" onclick="addToCart(' . $db_product_id . ')">Add To Cart</button> </td>';
 		$output .= '</tr>';
 	}
 }
@@ -113,12 +111,15 @@ body{
 	font-family:Starborn; 
 	src:url('STARBORN.TTF');
 }
+.priceInput {
+	width: 50px;
+}
 </style>
 </head>
 
 <body>
 <p style="text-align: right"><a href="cart.php">View cart</a></p>
-<center><p style="font-size:100px; font-family:Starborn;">Fantasy Shop</p></center>
+<center><p style="font-size:100px; font-family:Starborn; margin-top:0px;">Fantasy Shop</p></center>
 
 	<center><p>From below dropdown menus you can apply filters</p><center>
 	  <!--filter forms-->
@@ -143,16 +144,22 @@ body{
 				</select>
 				<button type="submit">Apply</button>
 			</form>
-
+			<br>
 			<form action="" method="post">
-				<select name="price">
-					<option value="">Select Price Range</option>
-					<option value="range1">0 - 100</option>
-					<option value="range2">100 - 200</option>
-					<option value="range3">200 - 300</option>
-					<option value="range4">300 - 400</option>
-					<option value="range5">400 - 500</option>
-					<option value="range6">500 - 600</option>
+				Select Price Range: <br>
+				Low: <input type="number" name="low" class="priceInput">
+				High: <input type="number" name="high" class="priceInput">
+				<button type="submit" name="price" value="1">Apply</button>
+			</form>
+			<br>
+			<form action="" method="post">
+				<select name="location">
+					<option value="">Select Location</option>
+					<option value="RPG Emporium">RPG Emporium</option>
+					<option value="The Fantasy Corner">The Fantasy Corner</option>
+					<option value="The Heros Respite">The Hero's Respite</option>
+					<option value="Heros last stand">Hero's last stand</option>
+					<option value="Potions and curiosities">Potions and curiosities</option>
 				</select>
 				<button type="submit">Apply</button>
 			</form>
@@ -172,6 +179,9 @@ body{
 				data: data,
 				success: function (data, status) {
 					console.log(status);
+					if (status == "success") {
+						$("#btn-" + id).html("Added!");
+					}
 				}
 			});
 		}
